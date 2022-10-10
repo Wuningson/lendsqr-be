99fa5c5bb38c72c7d01052fb44dfe33f63ad842b
Nicholas Ifeajika <nickifeajika@gmail.com>
2022-10-10 19:52:43 +0100

    model fix

--- database/models.ts
+++ database/models.ts
@@ -11,19 +11,15 @@ db.schema.createTable('user', function (table) {
 
 db.schema.createTable('transaction', function (table) {
     table.increments('transaction_id').primary();
-    table.string('transaction_type', 100)
-    table.integer('user_id').unsigned()
-    table.string('username', 100)
-    table.string('email', 30)
+    table.string('transaction_type', 100) // transfer, deposit, withdrawal
+    table.float('amount')
     table.foreign('user_id').references('user.user_id').deferrable('deferred')
 });
 
-db.schema.createTable('', function (table) {
-    table.increments('id').primary();
-    table.string('first_name', 100)
-    table.string('last_name', 100)
-    table.string('username', 100)
-    table.string('email', 30)
+db.schema.createTable('transfer', function (table) {
+    table.primary(['sender_id', 'transaction_id']) // composite table
+    table.foreign('receiver_id').references('user.user_id')
+    table.float('amount')
 });
 
 export default db;
